f1ec6661800ce047984e568341a49aa3a7be142c
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-10-05 19:23:19 +0300

    debounced fn

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,14 @@
-
+const filters = '?fields=name,capital,population,flags.svg,languages';
+import { Notify } from 'notiflix';
+export function fetchCountries(name) {
+  fetch(`https://restcountries.com/v2/name/${name}${filters}`)
+    .then(response => {
+      if (!response.ok) {
+        Notify.failure('Oops, there is no country with that name');
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+}
--- src/index.js
+++ src/index.js
@@ -3,23 +3,15 @@ import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const filters = '?fields=name,capital,population,flags.svg,languages';
 const DEBOUNCE_DELAY = 300;
 const inputRef = document.querySelector('input#search-box');
 
 inputRef.addEventListener('input', onInputChange);
+const debouncedSearch = debounce(fetchCountries, DEBOUNCE_DELAY);
 
 function onInputChange(e) {
   const query = e.currentTarget.value;
   const structuredQuery = query.trim().toLowerCase();
   console.log(structuredQuery);
-  fetch(`https://restcountries.com/v2/name/${structuredQuery}${filters}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Sorry, we coundn`t find your country');
-      }
-      return response.json();
-    })
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+  debouncedSearch(structuredQuery);
 }
