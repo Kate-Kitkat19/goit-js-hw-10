43f5517b209800a3f64d84282b9cd1160dd5b731
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-10-05 17:53:22 +0300

    function working on input

--- src/index.js
+++ src/index.js
@@ -2,18 +2,8 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-const BASE_URL = 'https://restcountries.com/v2/name/';
+
 const filters = '?fields=name,capital,population,flags.svg,languages';
-
-// function fetchCountries(name) {
-//   fetch(`${BASE_URL}${name}${filters}`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   });
-// }
-
 const DEBOUNCE_DELAY = 300;
 const inputRef = document.querySelector('input#search-box');
 
@@ -23,7 +13,13 @@ function onInputChange(e) {
   const query = e.currentTarget.value;
   const structuredQuery = query.trim().toLowerCase();
   console.log(structuredQuery);
-  // fetchCountries(structuredQuery)
-  //   .then(data => console.log(data))
-  //   .catch(err => console.log(err));
+  fetch(`https://restcountries.com/v2/name/${structuredQuery}${filters}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Sorry, we coundn`t find your country');
+      }
+      return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
 }
