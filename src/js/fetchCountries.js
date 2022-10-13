ca8fcde40f06ccfa171bba28e2ff062cfb905d3c
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-10-13 20:03:29 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,7 @@
 const filters = '?fields=name,capital,population,flags,languages';
 import { Notify } from 'notiflix';
-export function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v2/name/${name}${filters}`).then(
+export async function fetchCountries(name) {
+  return await fetch(`https://restcountries.com/v2/name/${name}${filters}`).then(
     response => {
       if (!response.ok) {
         Notify.failure('Oops, there is no country with that name');
